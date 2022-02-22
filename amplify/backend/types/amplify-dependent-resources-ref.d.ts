02dd400246866e5231d78002f26fafd5b5a82dd0
dnslusser <dslusser@madisoncollege.edu>
2022-02-22 15:32:19 -0600

    ran amplify push...
    and tested in console by running amplify console api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "cryptoapp00775e45": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "cryptoapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
